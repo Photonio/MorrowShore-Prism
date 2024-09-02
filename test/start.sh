f48b84e1258035f21979707edc2b1f83afe8e862
DarkStar <darkstar@unisocial.net>
2024-09-02 16:07:36 -0500

    0

--- test/start.sh
+++ test/start.sh
@@ -1,5 +1,5 @@
 source key
-source rm\ old
+source rm-old
 
 echo $twitch_url
 
@@ -7,8 +7,8 @@ all(){
   docker run -it -p 1935:1935 --name prism -e TWITCH_URL="<twitch server>" -e TWITCH_KEY="<twitch key>" -e FACEBOOK_KEY="<facebook key>" -e YOUTUBE_KEY="<youtube key>" -e TROVO_KEY="<trovo key>" -e KICK_KEY="<kick key>" -e RTMP1_URL="<custom RTMP1 server>" -e RTMP1_KEY="<custom RTMP1 key>" -e RTMP2_URL="<custom RTMP2 server>" -e RTMP2_KEY="<custom RTMP2 key>" -e RTMP3_URL="<custom RTMP3 server>" -e RTMP3_KEY="<custom RTMP3 key>" prism
 }
 
-minimal(){
-  docker run -it -p 1935:1935 --name prism -e TWITCH_URL="${twitch_url}" -e TWITCH_KEY="${twitch_key}" prism
+several(){
+  docker run -it -p 1935:1935 --name prism -e TWITCH_URL="${twitch_url}" -e TWITCH_KEY="${twitch_key}" -e FACEBOOK_KEY=${facebook_key} prism
 }
 
 openPort(){
@@ -17,4 +17,4 @@ openPort(){
 
 rmOld
 openPort
-minimal
+several
